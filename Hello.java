40b6ee34fee62d37dee064f244caa6d63bab6728
Sourabh Mandal <sourabh_mandal@persistent.com>
2016-09-15 14:52:51 +0530

    Commit_Solo

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is a Maven First Job Example, To Be Deployed with Jenkins over git" );
+    }
+}
